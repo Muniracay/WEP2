fef0432193a0bf2825e3a5c611adac081cfb80e4
Muniracay <nirwanmahdiarana93@gmail.com>
2022-08-03 21:33:52 +0700

    update foto commit

--- fragment/sidebar.php
+++ fragment/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Sidebar user panel (optional) -->
 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
     <div class="image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <img src="dist/img/user8-128x128.jpg" class="img-circle elevation-2" alt="User Image">
     </div>
     <div class="info">
         <a href="#" class="d-block"><?=$_SESSION['login']['nama_operator'];?></a>
